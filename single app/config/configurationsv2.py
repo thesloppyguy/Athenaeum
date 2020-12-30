e51105867a5276735410735cac1241b2b73499d7
thesloppyguy <72688625+thesloppyguy@users.noreply.github.com>
2020-12-30 16:26:08 +0530

    Add files via upload

---
+++ single app/config/configurationsv2.py
@@ -0,0 +1,8 @@
+config = {
+    "apiKey": "AIzaSyARmwwgJYtbta6buF5avzIt9sBeykaJjt8",
+    "authDomain": "login-75eda.firebaseapp.com",
+    "databaseURL": "https://login-75eda-default-rtdb.firebaseio.com",
+    "projectId": "login-75eda",
+    "storageBucket": "login-75eda.appspot.com",
+    "messagingSenderId": "380517597845"
+}
