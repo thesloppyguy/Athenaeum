7585e38237a50972411d31f76c7f1a171dd9c898
thesloppyguy <72688625+thesloppyguy@users.noreply.github.com>
2020-12-30 16:31:02 +0530

    Delete configurations.py

--- single app/configurations.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "apiKey": "AIzaSyBJEvnrUiZtdSEgVYPTOpwJXQRc691T0_o",
-    "authDomain": "athenaeum-aed69.firebaseapp.com",
-    "databaseURL": "https://athenaeum-aed69-default-rtdb.firebaseio.com",
-    "projectId": "athenaeum-aed69",
-    "storageBucket": "athenaeum-aed69.appspot.com",
-    "messagingSenderId": "807794797425"
-}
