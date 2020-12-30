d66a1a439aaef8478638c0eed46e711d9cd5b2d6
thesloppyguy <72688625+thesloppyguy@users.noreply.github.com>
2020-12-30 16:30:49 +0530

    Add files via upload

--- single app/app.py
+++ single app/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for,request
+from flask import Flask, render_template, url_for,request,redirect
+from config import configurations
+from config import configurationsv2
 import pyrebase
-import configurations
 import pymongo
 import bcrypt
+import time
 #from subpro import givVal
 #from login import routes 
 
@@ -11,15 +13,70 @@ import bcrypt
 firebsevar = pyrebase.initialize_app(config=configurations.config)
 db = firebsevar.database()
 
+
+login = pyrebase.initialize_app(config=configurationsv2.config)
+log = login.database() 
+auth = login.auth()
+
+token = ""
+
 app = Flask(__name__)
 
 
 @app.route('/',methods=[ 'POST' , 'GET'])
 def index():
+    eu = "Incorrent Email or Password"
+    ea = "Email is already registered"
+    
+    if request.method == 'POST':
+        
+        if  request.form['comment_id']=='0':
+            
+            email = request.form['login-email']
+            password = request.form['login-pass']
+                        
+            try:
+                auth.sign_in_with_email_and_password(email, password) 
+                return redirect('/home')
+            except:
+                return render_template('Athenaeum.html', comment=eu)
+            
+        elif request.form['comment_id']=='1':
+            
+            email = request.form['register-email']
+            password = request.form['register-pass']
+        
+            try:
+                auth.create_user_with_email_and_password(email, password)
+                time.sleep(3)
+                auth.sign_in_with_email_and_password(email, password)
+                time.sleep(3)
+                return redirect('/profile')
+            except:
+                return render_template('Athenaeum.html', comment=ea)
+    
     return render_template('Athenaeum.html')
 
-@app.route('/profile')
+@app.route('/profile', methods=['POST', 'GET'])
 def Profile():
+    if request.method == 'POST':
+        first_name          = request.form['first_name']
+        last_name           = request.form['last_name']
+        area_code           = request.form['area_code']
+        phone               = request.form['phone']
+        p_name              = request.form['p_name']
+        p_email             = request.form['p_email']
+        p_phone             = request.form['p_phone']
+        year                = request.form['year']
+        subject             = request.form['subject']
+        beg                 = request.form['rdiobtn']
+        token= auth.current_user['localId']
+        print(token)
+        profile={"firstname":first_name, "lastname":last_name,"area_code":area_code,"phone":phone,"p_name":p_name,"p_email":p_email,"p_phone":p_phone,"year":year,"subject":subject,"beg":beg}                                                                            
+        
+        log.child(token).push(profile)
+        return redirect('/home')
+    
     return render_template('Profile.html')
 
 @app.route('/home')
@@ -53,9 +110,10 @@ def foo():
         third = request.form['third']
         
         ans = {"a1": first, "a2": second, "a3": third}
-        result = db.child("/answers").push(ans)
+        db.child("/answers").push(ans)
         
     #k=givval()
+    k=5
     if t==0:  
         return render_template('Page-4.html')
     else:
@@ -64,3 +122,4 @@ def foo():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
